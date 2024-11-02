035bfe7f33148e610b664b3807562d5eb132584f
Seres Zoltán <seres.zoltan@kdiv.hu>
2024-11-02 16:26:37 +0100

    cols

--- resources/views/fight/index.blade.php
+++ resources/views/fight/index.blade.php
@@ -14,6 +14,7 @@
     }
    
     .title-fight {
+        /* A címmeccs kártyájának árnyéka aranyozott legyen és pulzáljon*/
     background: linear-gradient(135deg, #0f1a1f, #19292f, #223740) !important; /* Sötét, elegáns átmenet */
     color: white  !important; /* Sötét szöveg a kontrasztért */
     border-radius: 6px;
@@ -23,8 +24,52 @@
     font-size: 0.9em;
     letter-spacing: 0.5px;
     text-transform: uppercase;
-    transition: transform 0.3s ease, box-shadow 0.3s ease;
-    box-shadow: 2px 0px 8px #000;
+    transition: transform 0.1s ease, box-shadow 0.1s ease;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+    cursor: pointer;
+    outline: none;
+    box-shadow: 0 0 0px rgba(255, 0, 0, 0.5);
+    position: relative;
+    overflow: hidden;
+    animation: glow 1.5s ease-in-out infinite;
+}
+
+.title-fight:after {
+    content: "";
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        border-radius: 5px;
+        opacity: 0;
+        animation: pulse-bg 1.5s ease-in-out;
+        transform: scale(1.02);
+}
+
+@keyframes glow {
+
+0%,
+100% {
+    box-shadow: 0 0 15px #c0974a55;
+}
+
+50% {
+    box-shadow: 0 0 25px #c0974a55;
+}
+}
+
+
+@keyframes pulse-bg {
+
+0%,
+100% {
+    opacity: 0;
+}
+
+50% {
+    opacity: 1;
+}
 }
 
 .title-fight:hover {
@@ -41,6 +86,20 @@
     font-weight: 500;
     font-size: 0.9em;
     transition: background-color 0.3s ease, border-left-color 0.3s ease;
+}
+
+.pirossarok {
+    /* A nyertes sarok színe a meccs után piros színátmenetet kap */
+    background: linear-gradient(135deg, #ff000055, #212121, #212121) !important;
+    color: white !important;
+    border-left-color: #ff0000;
+}
+
+.keksarok {
+    /* A győztes sarok színe a meccs után kék színátmenetet kap */
+    background: linear-gradient(135deg,  #0000ff55, #212121, #212121) !important;
+    color: white !important;
+    border-left-color: #0000ff;
 }
 
 .felnott:hover {
@@ -129,7 +188,7 @@
                                     $i--
                                 @endphp
                                 <div class="panel panel-default">
-                                    <div class="@if($fight->title_fight == 1) title-fight @endif @if($fight->agegroup == 'Felnőtt' && $fight->title_fight == 0) felnott @endif @if($fight->agegroup == 'Gyerek') gyerek @endif" >
+                                    <div class="@if (isset($winner) && $red->id == $winner->id) pirossarok @endif  @if (isset($winner) && $blue->id == $winner->id) keksarok @endif @if($fight->title_fight == 1) title-fight @endif @if($fight->agegroup == 'Felnőtt' && $fight->title_fight == 0) felnott @endif @if($fight->agegroup == 'Gyerek') gyerek @endif" >
                                         <div class="row">
                                             <div class="col-md-3">
                                                 <a data-toggle="collapse" data-parent="#fightsAccordion" href="#collapse{{$fight->id}}">
@@ -153,8 +212,9 @@
                                                 @endif
                                             </div>
                                             <div class="col-md-3">
-                                                <p>
-                                                <h5>{{$red->name}} - {{ \Carbon\Carbon::parse($red->date_of_birth)->age }} <img
+                                                <p style="color: #ff0000">Piros sarok:</p>
+                                            <p>
+                                                <h5><a style="color:white;" href="{{route('fighters.show', $red->id)}}">{{ $red->name}}</a> - {{ \Carbon\Carbon::parse($red->date_of_birth)->age }} <img
                                                         width="25px"
                                                         src="{{ asset('/storage/flags/4x3/' . \App\Models\Fighter::getFlag($red->id))}}">
                                                 </h5>
@@ -166,8 +226,9 @@
                                                 <p><strong><span class="text-color">Vereség:</strong> {{$red->losses}}</p>
                                             </div>
                                             <div class="col-md-3">
+                                                <p style="color: #0000ff">Kék sarok:</p>
                                             <p>
-                                                <h5>{{$blue->name}} - {{ \Carbon\Carbon::parse($blue->date_of_birth)->age }} <img
+                                                <h5><a style="color:white;" href="{{route('fighters.show', $blue->id)}}">{{ $blue->name}}</a> - {{ \Carbon\Carbon::parse($blue->date_of_birth)->age }} <img
                                                         width="25px"
                                                         src="{{ asset('/storage/flags/4x3/' . \App\Models\Fighter::getFlag($blue->id))}}">
                                                 </h5>
